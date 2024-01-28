f590871cb2e8777baa17e2dcbf466f6b8ff4d8dd
Svitlana <svitlana.grytsai@gmail.com>
2024-01-29 00:14:53 +0100

    feat: start to implement a function

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -374,8 +374,19 @@ function rotateMatrix(/* matrix */) {
  *  [2, 9, 5, 9]    => [2, 5, 9, 9]
  *  [-2, 9, 5, -3]  => [-3, -2, 5, 9]
  */
-function sortByAsc(/* arr */) {
-  throw new Error('Not implemented');
+function sortByAsc(arr) {
+  const sortedArray = arr;
+  for (let i = 0; i < arr.length - 1; i += 1) {
+    for (let j = 0; j < arr.length - i - 1; j += 1) {
+      if (sortedArray[j] > sortedArray[j + 1]) {
+        [sortedArray[j], sortedArray[j + 1]] = [
+          sortedArray[j + 1],
+          sortedArray[j],
+        ];
+      }
+    }
+  }
+  return sortedArray;
 }
 
 /**
